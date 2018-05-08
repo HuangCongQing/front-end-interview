7041c2e28a1438102c1781253a4d2c002fd60837
HuangChongqing <1756260160@qq.com>
2018-05-08 09:39:22 +0800

    AJAX

---
+++ codeSegment/ajax.js
@@ -0,0 +1,37 @@
+// 0 － （未初始化）还没有调用send()方法
+// 1 － （载入）已调用send()方法，正在发送请求
+// 2 － （载入完成）send()方法执行完成，已经接收到全部响应内容
+// 3 － （交互）正在解析响应内容
+// 4 － （完成）响应内容解析完成，可以在客户端调用了
+// 对于readyState的这五种状态
+
+var xmlhttp = null;//声明一个变量，用来实例化XMLHttpRequest对象
+if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();// 新版本的浏览器可以直接创建XMLHttpRequest对象
+}
+
+else if (window.ActiveXObject) {
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");// IE5或IE6没有XMLHttpRequest对象，而是用的ActiveXObject对象
+}
+
+
+if (xmlhttp != null) {
+    xmlhttp.onreadystatechange = state_Change;//指定响应函数为state_Change
+    xmlhttp.open("GET", "/example/xdom/note.xml", true);//指定请求，这里要访问在/example/xdom路径下的note.xml文件，true代表的使用的是异步请求
+    xmlhttp.send(null);//发送请求
+}
+else {
+    alert("Your browser does not support XMLHTTP.");
+}
+
+//创建具体的响应函数state_Change
+function state_Change() {
+    if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+            // 这里应该是函数具体的逻辑
+        }
+        else {
+            alert("Problem retrieving XML data");
+        }
+    }
+}
